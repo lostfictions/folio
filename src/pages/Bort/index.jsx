ea8f8329391a5f63ed641f9bac4fe38463ca2060
lostfictions <lostfictions@users.noreply.github.com>
2018-03-02 03:07:52 -0500

    bort, but no description

--- src/pages/Bort/index.jsx
+++ src/pages/Bort/index.jsx
@@ -2,11 +2,11 @@ import React, { Component } from "react";
 
 import Gallery from "../Gallery";
 
-import gang0 from "./gangbot-0.jpg";
-import gang1 from "./gangbot-1.jpg";
-import gang2 from "./gangbot-2.jpg";
+// import gang0 from "./gangbot-0.jpg";
+// import gang1 from "./gangbot-1.jpg";
+// import gang2 from "./gangbot-2.jpg";
 
-const images = [gang0, gang1, gang2];
+// const images = [gang0, gang1, gang2];
 
 class Bort extends Component {
   render() {
@@ -18,6 +18,12 @@ class Bort extends Component {
         <h3 className="title-desc title-desc-last">
           Node.js, Discord, Slack, Electron
         </h3>
+        <iframe
+          src="https://doam.keybase.pub/"
+          width={720}
+          height={720}
+          frameBorder={0}
+        />
         <Gallery images={[]} />
       </div>
     );
